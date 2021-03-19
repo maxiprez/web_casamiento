0a78cee3cc99a33866294e69d125e92382edd9ea
Maxi Prez <maxi.prez@gmail.com>
2021-03-18 23:02:26 -0300

    photo gallery added

---
+++ js/pictures.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function(e) {
+ //console.log(e.target);
+
+
+ if(e.target.classList.contains("gallery-item")){
+     const src = e.target.getAttribute("src");
+     //console.log(src);
+     document.querySelector(".modal-img").src = src;
+     const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
+     myModal.show();
+ }
+
+});
